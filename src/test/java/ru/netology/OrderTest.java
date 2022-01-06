1797a4b9c5588e9ef94c64f0101e8dae2bb4a1db
Anna Sotnikova <ruceba77@gmail.com>
2022-01-06 18:48:27 +0600

    initial commit

---
+++ src/test/java/ru/netology/OrderTest.java
@@ -0,0 +1,47 @@
+package ru.netology;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class OrderTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldSubmitRequestSuccess() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иван Петров");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+71234567895");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.tagName("button")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+}
